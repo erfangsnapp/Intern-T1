54840422b2f0d5119b20daac55170f17adcbb858
Erfan Gooneh <Erfan.Gooneh@zoodfood.ir>
2024-02-08 22:53:03 +0330

    Adding login View

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,1 +1,8 @@
-<p>salam</p>
+<div class="log-form">
+  <h2>Login to your account</h2>
+  <form>
+    <input type="text" title="username" placeholder="username" /><br>
+    <input type="password" title="username" placeholder="password" /><br>
+    <button type="submit">Login</button>
+  </form>
+</div>

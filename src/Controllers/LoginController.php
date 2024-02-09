0183dad93961eeea2e836e2f015e634da47b7135
erfangsnapp <159261865+erfangsnapp@users.noreply.github.com>
2024-02-09 16:04:34 +0330

    Create LoginController.php

---
+++ src/Controllers/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ErfanGooneh\T1\Controllers;
+use ErfanGooneh\T1\Controller;
+use ErfanGooneh\T1\Models;
+use ErfanGooneh\T1\Models\User;
+
+class LoginController extends Controller
+{
+    public function index(){
+        if($_SERVER['REQUEST_METHOD'] === 'GET')
+            $this->render('login') ;
+        else if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $user = new User($username, $password);
+            $this->render('home', ['is_admin'=>$user->is_admin()]) ; 
+        }
+    }
+}

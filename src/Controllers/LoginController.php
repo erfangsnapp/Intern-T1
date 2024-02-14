082d95adbf9c3b8cc1c4c7b91eb5fef60f0584a8
erfangsnapp <159261865+erfangsnapp@users.noreply.github.com>
2024-02-14 16:48:04 +0000

    Adding Invalid username error to LoginController

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -13,11 +13,16 @@ class LoginController extends Controller
         else if($_SERVER['REQUEST_METHOD'] === 'POST'){
             $username = $_POST['username'];
             $password = $_POST['password'];
-            $user = User::get($username); 
-            if($password !== $user->password)
+            if(!isset(User::all()[$username])){
+                $this->render('login', ['error' => 'Invalid username']);
+            }
+            if($password !== $user->password){
                 $this->render('login', ['error' => 'Invalid username or password']);
-            else
+            }
+            else{
+                $user = User::get($username);
                 $this->render('home', ['is_admin'=>$user->is_admin()]) ; 
+            }
         }
     }
 }
